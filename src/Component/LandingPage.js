5e7b375a562cda9665a8450e07c4b7e10973ac6e
Shiva-dwivedi-au9 <67583462+Shiva-dwivedi-au9@users.noreply.github.com>
2021-01-18 23:31:11 +0530

    added build

--- src/Component/LandingPage.js
+++ src/Component/LandingPage.js
@@ -1,19 +1,31 @@
 import React from 'react'
+import {Spring} from 'react-spring/renderprops'
 import './Landing.css'
 import img from '../Images/deadpool.jpg'
 
 
 export default function LandingPage() {
     return (
-        <div className="banner">
-            <img src={img} />
-            <div className="heading">
-                <h1>WELCOME TO </h1>
-                <h1 className="title">DAILY MOVIE MANIA</h1>
-                <h2>Explore the Big screen and the Small screen entertainment here</h2>
-            </div>
-           
-        </div>
+        <Spring
+          
+        from = { {opacity: 0 , margin: -500}}
+        to = {{opacity: 1 , margin: 0}}
+       >
+       {props =>  ( 
+           <div style={props}>
+           <div className="banner">
+           <img src={img} />
+           <div className="heading">
+               <h1>WELCOME TO </h1>
+               <h1 className="title">DAILY MOVIE MANIA</h1>
+               <h2>Explore the Big screen and the Small screen entertainment here</h2>
+           </div>
+          
+       </div>
+           </div>
+       )}
+       </Spring>
     )
 }
 
+ 
