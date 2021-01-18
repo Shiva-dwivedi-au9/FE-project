3b5feb23891d3b8d785194c833b16a83ec1023b8
Shiva-dwivedi-au9 <67583462+Shiva-dwivedi-au9@users.noreply.github.com>
2021-01-19 01:11:22 +0530

    added build

--- src/Component/LandingPage.js
+++ src/Component/LandingPage.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import {Spring} from 'react-spring/renderprops'
 import './Landing.css'
-import img from '../Images/deadpool.jpg'
-
 
 export default function LandingPage() {
     return (
@@ -14,11 +12,15 @@ export default function LandingPage() {
        {props =>  ( 
            <div style={props}>
            <div className="banner">
-           <img src={img} />
+           <iframe width="560" height="315" src="https://www.youtube.com/embed/5OqiOBQfnNM?autoplay=1&playlist=5OqiOBQfnNM&controls=0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
            <div className="heading">
                <h1>WELCOME TO </h1>
                <h1 className="title">DAILY MOVIE MANIA</h1>
-               <h2>Explore the Big screen and the Small screen entertainment here</h2>
+               <div className="sub-heading">
+                    <h2>Explore the Big screen and the Small screen entertainment here</h2>
+                    <h2>Scroll or Swipe up to check what's trending today</h2>
+               </div>
+               
            </div>
           
        </div>
